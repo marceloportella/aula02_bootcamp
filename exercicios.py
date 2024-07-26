6ebba75b6778d89426510b3efa9cf01ded886457
Marcelo Portella <marcelo.portella@outlook.com>
2024-07-26 08:29:11 -0300

    exercicio area do circulo

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,12 @@
-numero01 =int(input("Digite o segundo numero "))
-numero02=int(input("Digite o segundo numero "))
-resultado = numero01 // numero02 # // divisao inteira
+import math
+#numero01 =int(input("Digite o segundo numero "))
+#numero02=int(input("Digite o segundo numero "))
+#resultado = numero01 // numero02 # // divisao inteira
 
-print(resultado)
+#print(resultado)
 
+
+raio = float(input("Digite o raio : "))
+area = math.pi * raio ** 2
+
+print (area)
